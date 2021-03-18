dbde58ccb0d2d33da5f460a8cd694276221981d2
Shaun Quick <shaun.quick@btinternet.com>
2021-03-18 17:09:45 +0000

    set button nrs correctly

--- emulationstation/scripts/game-start/001_game-start.sh
+++ emulationstation/scripts/game-start/001_game-start.sh
@@ -9,14 +9,14 @@ pkill -f set-ultimateio-leds.py
 case "$2" in
         "romname1")
         echo "running generic romname1 script" >> ~/.emulationstation/scripts/output.log
-	python3  ~/ipacultimateio/set-ultimateio-leds.py 1buttons.json >> ~/.emulationstation/scripts/output.log
+	python3  ~/ipacultimateio/set-ultimateio-leds.py 1buttons.json >> ~/.emulationstation/scripts/output.log &
         ;;
         "romname2")
         echo "running generic romname2 script" >> ~/.emulationstation/scripts/output.log
-	python3  ~/ipacultimateio/set-ultimateio-leds.py 2buttons.json >> ~/.emulationstation/scripts/output.log
+	python3  ~/ipacultimateio/set-ultimateio-leds.py 2buttons.json >> ~/.emulationstation/scripts/output.log &
         ;;
         *)
         echo "running generic game-start script" >> ~/.emulationstation/scripts/output.log
-        python3  ~/ipacultimateio/set-ultimateio-leds.py allbuttons.json >> ~/.emulationstation/scripts/output.log
+        python3  ~/ipacultimateio/set-ultimateio-leds.py allbuttons.json >> ~/.emulationstation/scripts/output.log &
         ;;
 esac
